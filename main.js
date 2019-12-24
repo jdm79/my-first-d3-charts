5a24db3d1aa9d0bbf2801b2ed085741223b08904
jdm79 <james.malvern@gmail.com>
2019-12-24 18:29:40 +0000

    connects the external data from csv and populates the rectangles one by one

---
+++ main.js
@@ -0,0 +1,24 @@
+d3.csv("data/ages.csv").then(function(data){
+  data.forEach(function(d){
+    d.age = +d.age
+  })
+  const svg = d3.select("#chart-area").append("svg")
+      .attr("width", 500)
+      .attr("height", 500)
+
+  const rects = svg.selectAll("rect")
+      .data(data)
+
+  rects.enter()
+      .append("rect")
+          .attr("x", function(d,i){
+            return (i * 50) + 25
+          })
+          .attr("y", 0)
+          .attr("width", 20)
+          .attr("height", function(d){
+            return d.age * 3
+          })
+          .attr("fill", "red")
+  console.log(data)
+})
