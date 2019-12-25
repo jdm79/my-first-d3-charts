919c03fb9a0bcb53ad09aa88e93683ffe9acd0aa
jdm79 <james.malvern@gmail.com>
2019-12-25 20:21:43 +0000

    copies in the graph which has inverted the bars to sit flush on the bottom of the graph now

--- js/main.js
+++ js/main.js
@@ -1,24 +1,86 @@
-d3.csv("data/ages.csv").then(function(data){
-  data.forEach(function(d){
-    d.age = +d.age
-  })
-  const svg = d3.select("#chart-area").append("svg")
-      .attr("width", 500)
-      .attr("height", 500)
+/*
+*    main.js
+*    Mastering Data Visualization with D3.js
+*    Project 1 - Star Break Coffee
+*/
 
-  const rects = svg.selectAll("rect")
-      .data(data)
+const paddingInner = 0.3, paddingOuter = 0.4
 
-  rects.enter()
-      .append("rect")
-          .attr("x", function(d,i){
-            return (i * 50) + 25
-          })
-          .attr("y", 0)
-          .attr("width", 20)
-          .attr("height", function(d){
-            return d.age * 3
-          })
-          .attr("fill", "red")
-  console.log(data)
+const margin = { top: 20, right: 10, bottom: 100, left: 100}
+let width = 600 - margin.left - margin.right, 
+      height = 400 - margin.top - margin.bottom
+
+const g = d3.select("#chart-area").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")") // appends group to canvas, shifts it
+
+g.append("text")
+    .attr("class", "x axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + 50)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .text("Star Break revenue 2019")
+
+g.append("text")
+    .attr("class", "y axis-label")
+    .attr("x", - (height / 2) )
+    .attr("y", - 60)
+    .attr("font-size", "20px")
+    .attr("text-anchor", "middle")
+    .attr("transform", "rotate(-90)") // this makes the title run vertically down the y axis
+    .text("Revenue - $")
+
+
+d3.json("data/revenues.json").then(function(data){
+    data.forEach(function(d){
+        d.revenue = +d.revenue
+        d.profit = +d.profit
+    })
+    const x = d3.scaleBand()
+        .domain(data.map(function(d){
+            return d.month
+        }))
+        .range([0, width])
+        .paddingInner(paddingInner)
+        .paddingOuter(paddingOuter)
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, function(d){
+            return d.revenue
+        })])
+        .range([height, 0])
+
+    const xAxisCall = d3.axisBottom(x)
+    g.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxisCall)
+        // .selectAll("text")
+        //     .attr("y", "10")
+        //     .attr("x", "-5")
+        //     .attr("text-anchor", "end")
+        //     .attr("transform", "rotate(-40)")
+   
+    const yAxisCall = d3.axisLeft(y)
+        // only use this if you want a measurement after the integer (e.g. 100m)
+        // .tickFormat(function(d){
+        //     return d + "m"
+        // })
+    g.append("g")
+        .attr("class", "y-axis")
+        .call(yAxisCall)
+        
+    const rects = g.selectAll("rect")
+        .data(data)
+
+    rects.enter()
+        .append("rect")
+            .attr("y", function(d){return y(d.revenue)})
+            .attr("x", function(d){return x(d.month)})
+            .attr("width", x.bandwidth)
+            .attr("height", function(d){return height - y(d.revenue)})
+            .attr("fill", "green");
 })
